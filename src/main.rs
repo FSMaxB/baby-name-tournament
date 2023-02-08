0f1575efc692e0c1e8e46b43b0d6e006017c769a
Max Bruckner <max@maxbruckner.de>
2023-02-08 22:19:19 +0100

    Print lines in file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-	println!("Hello, world!");
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+fn main() -> anyhow::Result<()> {
+	let cli = Cli::parse();
+	cli.run()
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+	name_list: PathBuf,
+}
+
+impl Cli {
+	pub fn run(self) -> anyhow::Result<()> {
+		let name_list = File::open(self.name_list)?;
+
+		for line in BufReader::new(name_list).lines() {
+			let line = line?;
+			println!("{line}");
+		}
+		Ok(())
+	}
 }
