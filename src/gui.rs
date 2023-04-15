57be6585e323b178d6baa18a70913271cfa20bc5
Max Bruckner <max@maxbruckner.de>
2023-04-16 00:53:17 +0200

    Replace custom NameList object with Relm component

--- src/gui.rs
+++ src/gui.rs
@@ -1,8 +1,9 @@
-use crate::gui::backend::Backend;
+use crate::gui::name_list::NameList;
 use crate::gui::runtime_thread::RuntimeThread;
-use libadwaita::gtk;
 use libadwaita::prelude::*;
-use relm4::{ComponentParts, ComponentSender, RelmApp, SimpleComponent};
+use relm4::{
+	gtk, Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmApp, SimpleComponent,
+};
 use sqlx::SqlitePool;
 use tokio::runtime::Runtime;
 
@@ -25,8 +26,7 @@ pub fn start(runtime: Runtime, database_pool: SqlitePool) -> anyhow::Result<()> 
 }
 
 struct Application {
-	runtime_handle: tokio::runtime::Handle,
-	database_pool: SqlitePool,
+	_name_list_controller: Controller<NameList>,
 }
 
 #[relm4::component]
@@ -43,8 +43,8 @@ impl SimpleComponent for Application {
 			gtk::Box {
 				set_orientation: gtk::Orientation::Vertical,
 
-				#[name(name_list)]
-				name_list::NameList {}
+				#[local]
+				name_list -> gtk::Box {}
 			}
 		}
 	}
@@ -54,17 +54,15 @@ impl SimpleComponent for Application {
 		root: &Self::Root,
 		_sender: ComponentSender<Self>,
 	) -> ComponentParts<Self> {
-		let backend = Backend::new(database_pool.clone(), runtime_handle.clone());
-		let model = Self {
-			runtime_handle,
-			database_pool,
-		};
+		let name_list_controller = NameList::builder()
+			.launch((runtime_handle.clone(), database_pool.clone()))
+			.detach();
+		let name_list = name_list_controller.widget().clone();
 		let widgets = view_output!();
 
-		widgets.name_list.initialize(backend);
-
+		let model = Self {
+			_name_list_controller: name_list_controller,
+		};
 		ComponentParts { model, widgets }
 	}
-
-	fn update(&mut self, _message: Self::Input, _sender: ComponentSender<Self>) {}
 }
--- src/gui/name_list.rs
+++ src/gui/name_list.rs
@@ -2,31 +2,90 @@ use crate::csv_parser::Gender;
 use crate::database;
 use crate::database::Name;
 use crate::gui::backend::Backend;
-use crate::gui::database_list_model::DatabaseView;
+use crate::gui::database_list_model::{DatabaseListModel, DatabaseView};
 use crate::gui::name_model::NameModel;
 use async_trait::async_trait;
-use libadwaita::glib::Object;
-use libadwaita::subclass::prelude::*;
-use libadwaita::{glib, gtk};
+use gtk::{prelude::*, ColumnViewColumn, Label, PolicyType, SignalListItemFactory, SingleSelection, Widget};
+use relm4::gtk;
+use relm4::{ComponentParts, ComponentSender, SimpleComponent};
 use sqlx::SqlitePool;
 
-mod implementation;
+pub struct NameList;
 
-glib::wrapper! {
-	pub struct NameList(ObjectSubclass<implementation::NameList>)
-		@extends gtk::Widget, gtk::Box,
-		@implements gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget;
-}
+#[relm4::component(pub)]
+impl SimpleComponent for NameList {
+	type Input = ();
+	type Output = ();
+	type Init = (tokio::runtime::Handle, SqlitePool);
 
-impl Default for NameList {
-	fn default() -> Self {
-		Object::new()
+	view! {
+		gtk::Box {
+			set_homogeneous: true,
+
+			gtk::ScrolledWindow {
+				set_hscrollbar_policy: PolicyType::Never,
+				set_propagate_natural_height: true,
+
+				#[name(column_view)]
+				gtk::ColumnView {
+					set_reorderable: false,
+					set_show_column_separators: true,
+					set_show_row_separators: true,
+				}
+			}
+		}
 	}
-}
 
-impl NameList {
-	pub fn initialize(&self, backend: Backend) {
-		self.imp().initialize(backend, NameListView);
+	fn init(
+		(runtime_handle, database_pool): Self::Init,
+		_root: &Self::Root,
+		_sender: ComponentSender<Self>,
+	) -> ComponentParts<Self> {
+		let widgets = view_output!();
+
+		let name_factory = SignalListItemFactory::new();
+		let gender_factory = SignalListItemFactory::new();
+
+		name_factory.connect_setup(|_, list_item| {
+			let name_label = Label::new(Some("<empty name>"));
+			list_item.set_child(Some(&name_label));
+			list_item
+				.property_expression("item")
+				.chain_property::<NameModel>("name")
+				.bind(&name_label, "label", Widget::NONE);
+		});
+		gender_factory.connect_setup(|_, list_item| {
+			let gender_label = Label::new(Some("<empty gender>"));
+			list_item.set_child(Some(&gender_label));
+			list_item
+				.property_expression("item")
+				.chain_property::<NameModel>("gender")
+				.bind(&gender_label, "label", Widget::NONE);
+		});
+
+		let name_column = ColumnViewColumn::builder()
+			.title("Name")
+			.resizable(true)
+			.factory(&name_factory)
+			.build();
+		let gender_column = ColumnViewColumn::builder()
+			.title("Gender")
+			.resizable(true)
+			.factory(&gender_factory)
+			.build();
+
+		let list_view = NameListView;
+		let list_model = DatabaseListModel::new(Backend::new(database_pool.clone(), runtime_handle.clone()), list_view);
+		let selection_model = SingleSelection::new(Some(list_model));
+
+		widgets.column_view.set_model(Some(&selection_model));
+		widgets.column_view.append_column(&name_column);
+		widgets.column_view.append_column(&gender_column);
+
+		ComponentParts {
+			model: NameList,
+			widgets,
+		}
 	}
 }
 
@@ -44,6 +103,6 @@ impl DatabaseView for NameListView {
 	async fn count(database_pool: &SqlitePool) -> u32 {
 		database::count_names(Gender::Both, database_pool)
 			.await
-			.expect("Faile to count names") as u32
+			.expect("Failed to count names") as u32
 	}
 }
